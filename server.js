90ba848cd631d11e23aa110780390b4e52312ea6
gill_pevzner <grigoripevzner@gmail.com>
2022-01-11 19:17:52 +0200

    mongo-connected

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const connectDB = require('./config/db');
+
+connectDB();
 
 const app = express();
 app.use(express.json());
