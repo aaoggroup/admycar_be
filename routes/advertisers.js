0ef04f2eb259bd46057271e4b3cbd0406f75f511
Aviad Shmuel <vocaviad@gmail.com>
2022-01-15 15:48:50 +0200

    fixed cloud

--- routes/advertisers.js
+++ routes/advertisers.js
@@ -1,32 +1,35 @@
 const express = require("express");
 const router = express.Router();
-const AdvertisersSchema = require('../models/Advertisers');
-const CampaignsSchema = require('../models/Campaigns');
-const { cloudinary, uploadToCloudinary } = require('../config/cloudinary');
+const AdvertisersSchema = require("../models/Advertisers");
+const CampaignsSchema = require("../models/Campaigns");
+const { cloudinary, uploadToCloudinary } = require("../config/cloudinary");
 
 router.post("/addCampaign", async (req, res) => {
   console.log(req.body);
-  const campaignsProperties = {
-    campaign_name: req.body.campaign_name,
-    advertiser_id: req.body.advertiser_id,
-    asset: req.body.asset,
-    current_bid: req.body.current_bid,
-    daily_budget: req.body.daily_budget,
-    total_budget: req.body.total_budget,
-    area: req.body.area,
-    date_created: Date.now(),
-  };
   try {
+    const imageURL = await uploadToCloudinary(req.body.asset);
+    console.log(imageURL);
+    const campaignsProperties = {
+      campaign_name: req.body.campaign_name,
+      advertiser_id: req.body.advertiser_id,
+      asset: imageURL,
+      current_bid: req.body.current_bid,
+      daily_budget: req.body.daily_budget,
+      total_budget: req.body.total_budget,
+      area: req.body.area,
+      date_created: String(Date.now()),
+    };
+
     const response = await CampaignsSchema.create(campaignsProperties);
     console.log(response);
-    //work with response
+    res.send(response);
   } catch (err) {
     console.error(err);
   }
 });
 
 const addCampaignToDB = async () => {
-  const response = await Advertisers.create()
-}
+  const response = await Advertisers.create();
+};
 
 module.exports = router;
